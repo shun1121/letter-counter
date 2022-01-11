859da605dd837dc6310b7a3b8366b05eb42dbe58
shun1121 <hikitanikun.ware80000@gmail.com>
2022-01-11 09:10:38 +0900

    added explanation

--- popup.js
+++ popup.js
@@ -1,9 +1,12 @@
 let addColor = document.getElementById('title')
 console.log(addColor)
+// btnにイベントリスナーを付与
 document.getElementById('btn').addEventListener("click", () => {
   chrome.storage.sync.get("color", ({ color }) => {
+    // background.jsでセットしたcolorをaddColorに適応
     addColor.style.color = color
   })
   let time = new Date().toTimeString()
+  // timeareaにtimeを代入
   document.getElementById('timearea').innerHTML = time
 })
